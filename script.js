0db09b6f45d6a7c97e23c91b7e5544482c5f4df0
Sekar243 <kinasekin6602@gmail.com>
2026-04-06 20:28:08 +0700

    Menambahkan fungsi button pesan dan button info kedai

--- script.js
+++ script.js
@@ -2,3 +2,11 @@ Date()
 
 let tanggal = new Date();
 document.getElementById("tanggal").innerHTML = "Hari ini: " + tanggal;
+
+function pesan(){
+    alert("Pesanan kamu sedang diproses 🍵");
+}
+
+function info(){
+    alert("Matcha Haven buka setiap hari 10.00 - 22.00");
+}
